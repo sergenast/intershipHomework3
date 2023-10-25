8eeef8edf002b5f0b2eb9ab61e9ea0a9c637d64f
sergenast <nastia0709@mail.ru>
2023-10-25 13:00:50 +0300

    homework3

---
+++ index.js
@@ -0,0 +1,30 @@
+const title = document.querySelector(".title");
+const text = document.querySelector(".text");
+const button = document.querySelector(".button");
+
+func = () => {
+    setTimeout( async () => { //сделала async/await, чтобы рендеринг срабатывал после промиса, подсмотрела у ребят в чатике))
+        await Promise.resolve().then(() => {
+            console.log ('promise 1');
+    });
+        title.style.color = 'red';       
+}, 0)
+
+     setTimeout(async () =>{
+        await Promise.resolve().then(()=>{
+            console.log('promise 4');                
+        })
+        text.textContent = "Новый текст";
+    }, 2000)
+
+    setTimeout(()=>{
+        Promise.resolve().then(()=>{
+            console.log('promise 2')
+        })
+        Promise.resolve().then(()=>{
+            console.log('promise 3')
+        })
+    }, 0)
+    
+}
+button.addEventListener("click", func);
